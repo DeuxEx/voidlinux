6732ffd5eb5d601179ffaab7f94f3b14bdeb0a94
DeuxEx <per@wennergrund.net>
2026-01-12 12:42:18 +0100

    Clean up troubleshooting section in services_howto.sh
    Removed troubleshooting instructions for desktop managers.

--- services_howto.sh
+++ services_howto.sh
@@ -1,23 +1,3 @@
-#Troubleshooting
-
-#if you have no desktop manager starting up , try:
-login
-sudo
-exec startxfce4
-
-#lightdm:
-#enable service:
-sudo ln -s /etc/sv/lightdm /var/service/
-sudo sv status lightdm
-#if fxce4 fails, try lxdm
-sudo xbps-install -S lxdm
-sudo ln -s /etc/sv/lxdm /var/service/
-#disable lightdm
-sudo rm /var/service/lightdm
-
-
-
-
 
 # Set up bluetooth autostart
 sudo ln -s /etc/sv/bluetoothd /var/service/
