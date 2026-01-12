ed608a66ad1eb2fc6c9d9a95b340731242870dc2
DeuxEx <per@wennergrund.net>
2026-01-12 21:59:17 +0100

    Add restore instructions to rsync_complete_backup.sh
    Added instructions for restoring backup data using rsync.

--- rsync_complete_backup.sh
+++ rsync_complete_backup.sh
@@ -37,6 +37,33 @@ sudo rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/
 
 
 
+#how to restore the backup data
+#boot inte a void live-USB/ISO
+
+sudo mount /dev/sdX1 /mnt         # Mount your new root partition
+sudo mkdir -p /mnt/backup
+sudo mount /dev/sdY1 /mnt/backup # Mount your backup drive
+
+# Example: If you backed up with: sudo rsync -aAX --delete / /mnt/backup/
+# Restore with:
+sudo rsync -aAX --delete /mnt/backup/ /mnt/
+
+for i in /dev /proc /sys; do sudo mount --bind $i /mnt$i; done # Bind essential dirs
+sudo chroot /mnt
+
+# Inside chroot:
+grub-install /dev/sdX # Install GRUB to the correct disk (not partition)
+grub-mkconfig -o /boot/grub/grub.cfg # Generate grub config
+exit # Exit chroot
+
+# Find your old UID/GID from backup (e.g., find /mnt/backup -user 1000)
+# Replace 1000 with your actual old ID, newuser with your new username
+sudo find /mnt -user 1000 -exec chown newuser:newgroup {} +
+
+# Unmount in reverse order from /mnt/backup to /mnt
+sudo umount /mnt/backup
+sudo umount /mnt
+reboot
 
 
 
@@ -46,10 +73,9 @@ sudo rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/
 
 
 
-
-
-sent 11,948,436,097 bytes  received 2,706,120 bytes  5,265,980.27 bytes/sec
-total size is 11,933,882,838  speedup is 1.00
+#backup data information
+#sent 11,948,436,097 bytes  received 2,706,120 bytes  5,265,980.27 bytes/sec
+#total size is 11,933,882,838  speedup is 1.00
 
 
 
